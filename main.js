b54af329648d8e5a52b132ae5b32de9b65615a82
mecnieri <abzogio@yahoo.com>
2019-01-28 09:01:39 +0400

    fix bugs, history

--- main.js
+++ main.js
@@ -1,50 +1,65 @@
-document.addEventListener("click", function (e) {
-    let id = e.target.parentNode.id
-    let score = Number(e.target.textContent)
-    let already = Number(document.getElementById(id + "sum").textContent)
-    let sum = already + score
+document.addEventListener("click", function(e) {
+  let id = e.target.parentNode.id;
+
+  let score = Number(e.target.textContent);
+  if (score.toString() != "NaN" && e.target.id.slice(-3) !== "sum") {
+    let already = Number(document.getElementById(id + "sum").textContent);
+    let sum = already + score;
     if (e.target.textContent == 0) {
-        sum = 0;
+      sum = 0;
     }
-    document.getElementById(id + "sum").innerHTML = sum
+    document.getElementById(id + "sum").innerHTML = sum;
+
+    let text = document.createTextNode(score);
+    let divNode = document.createElement("div");
+    divNode.appendChild(text);
+
+    document.getElementById(id + "history").appendChild(divNode)
+
+
+  }
+
 });
 // lom
-let addplayer = (e) => {
-    let name = e.target.name.value;
-    let divNode = document.createElement('div')
-    let h1Node = document.createElement('h1')
-    let text = document.createTextNode(name)
+let addplayer = e => {
+  let name = e.target.name.value;
+  let divNode = document.createElement("div");
+  let h1Node = document.createElement("h1");
+  let text = document.createTextNode(name);
 
-    let sum = document.createElement('h2')
-    let textS = document.createTextNode(0)
+  let sum = document.createElement("h2");
+  let history = document.createElement("h3")
+  let textS = document.createTextNode(0);
 
-    for (let i = 0; i <= 5; i++) {
-        let btnNodeP = document.createElement('button')
-        let btnNodeM = document.createElement('button')
-        let br = document.createElement('br')
-        let textM = document.createTextNode(10 * -i)
-        let textP = document.createTextNode("+" + (10 * i))
+  for (let i = 0; i <= 5; i++) {
+    let btnNodeP = document.createElement("button");
+    let btnNodeM = document.createElement("button");
+    let br = document.createElement("br");
+    let textM = document.createTextNode(10 * -i);
+    let textP = document.createTextNode("+" + 10 * i);
 
-        btnNodeP.appendChild(textP)
-        btnNodeM.appendChild(textM)
+    btnNodeP.appendChild(textP);
+    btnNodeM.appendChild(textM);
 
-        divNode.appendChild(btnNodeM)
-        if (i !== 0) {
-            divNode.appendChild(btnNodeP)
-        }
-        divNode.appendChild(br)
-
+    divNode.appendChild(btnNodeM);
+    if (i !== 0) {
+      divNode.appendChild(btnNodeP);
     }
+    divNode.appendChild(br);
+  }
+
+  sum.appendChild(textS);
+  history.appendChild(textS);
+  h1Node.appendChild(text);
+  divNode.appendChild(h1Node);
+
+  sum.setAttribute("id", name + "sum");
+  history.setAttribute("id", name + "history");
 
 
-    sum.appendChild(textS)
-    h1Node.appendChild(text)
-    divNode.appendChild(h1Node)
-
-    sum.setAttribute('id', name + "sum")
-
-    divNode.setAttribute('id', name)
-    divNode.appendChild(sum)
-    document.getElementsByClassName('players')[0].appendChild(divNode)
-    console.log();
-}
+  divNode.setAttribute("id", name);
+  divNode.appendChild(sum);
+  divNode.appendChild(history);
+  document.getElementsByClassName("players")[0].appendChild(divNode);
+   e.target.name.value = ""
+};
